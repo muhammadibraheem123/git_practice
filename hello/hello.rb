e56130e18324ea0c1817a7a0fcafb4eb6bf9f239
ibraheem <mibraheembadar@gmail.com>
2025-07-15 00:33:55 -0700

    Refactored hello.rb with greeter class

--- hello/hello.rb
+++ hello/hello.rb
@@ -1,4 +1,18 @@
-puts "hello,world"
+# hello.rb
 
-num = 2*3
-puts num  
+class Greeter
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    "Hello, #{@name}!"
+  end
+end
+
+name = ARGV.first || "World"
+greeter = Greeter.new(name)
+puts greeter.greet
+
+num = 2 * 3
+puts num
